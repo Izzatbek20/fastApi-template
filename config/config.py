c34861b0aa095a542d09d4763ec02093a01e93ee
Izzatbek Majidov <izzatbekmajidov200020@gmail.com>
2022-08-11 14:27:49 +0500

    ko`pitya qilib .env faylini yaratasiz

---
+++ config/config.py
@@ -0,0 +1,16 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanib .env faylini o`qib olamiz
+env = Env()
+env.read_env()
+
+# .env faylidagi ma`lumotlarni o`qib olamiz
+
+APP_NAME=env.str("APP_NAME")
+
+DB_CONNECTION=env.str("DB_CONNECTION")
+DB_HOST=env.str("DB_HOST")
+DB_PORT=env.str("DB_PORT")
+DB_DATABASE=env.str("DB_DATABASE")
+DB_USERNAME=env.str("DB_USERNAME")
+DB_PASSWORD=env.str("DB_PASSWORD")
