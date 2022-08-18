d12e8f5bf1d880f3ea579eb67ca30d8efa7d70f6
Izzatbek Majidov <izzatbekmajidov200020@gmail.com>
2022-08-18 17:00:55 +0500

    Post requestni validatsiya qilish

--- auth/schema.py
+++ auth/schema.py
@@ -1,5 +1,10 @@
 from typing import Union
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from database.db import SessionLocal
+from models.User import USER
+
+db = SessionLocal()
+
 
 class Token(BaseModel):
     access_token: str
@@ -21,3 +26,31 @@ class Register(BaseModel):
     password: str
     phone: str
     fullname: str
+
+    @validator('password')
+    def password_validate(cls, v):
+        if len(v) != 8:
+            raise ValueError('Parol 8 tadan kam bo`lmasligi kerak')
+        return v
+
+    @validator('username')
+    def username_validate(cls, v):
+        validate_my = db.query(USER).filter(
+            USER.username == v,
+        ).count()
+
+        if validate_my != 0:
+            raise ValueError('Bunday login aval ro`yxatga olingan.')
+        return v
+
+    @validator('phone')
+    def phone_validate(cls, v):
+        validate_my = db.query(USER).filter(
+            USER.phone == v,
+        ).count()
+
+        if validate_my != 0:
+            raise ValueError('Bunday telefon nomer aval ro`yxatga olingan.')
+        if len(v) != 13:
+            raise ValueError('Bu telefon raqam emas')
+        return v
