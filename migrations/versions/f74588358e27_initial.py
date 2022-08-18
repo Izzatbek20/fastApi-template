606e35aa0fa4b611493c9d233db306ad35518081
Izzatbek Majidov <izzatbekmajidov200020@gmail.com>
2022-08-18 13:35:50 +0500

    alembic orqali bazaga ma`lumot yozish

---
+++ migrations/versions/f74588358e27_initial.py
@@ -0,0 +1,24 @@
+"""Initial
+
+Revision ID: f74588358e27
+Revises: 
+Create Date: 2022-08-18 12:28:07.424234
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f74588358e27'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
