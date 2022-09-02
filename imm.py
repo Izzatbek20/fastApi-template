b599271574d685b3d87ce26504d063b8c5ec26ee
Izzatbek Majidov <izzatbekmajidov200020@gmail.com>
2022-09-02 18:27:11 +0500

    imm commands create

--- imm.py
+++ imm.py
@@ -1,5 +1,15 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
+parser.add_argument('name', help="Route")
+parser.add_argument("--m", '--model', help="Model yaratish uchun")
+parser.add_argument("--r", '--route', help="Route yaratish uchun")
+parser.add_argument("--s", '--schema', help="Chema yaratish uchun")
 args = parser.parse_args()
-print(args.echo)
+
+# route yaratish
+if args.name and args.route:
+    print('ergw')
+
+# model yaratish
+
+# schema yaratish
